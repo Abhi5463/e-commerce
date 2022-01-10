9c977d37128b1c907af8fc1294b491193c84f2ef
Abhi5463 <abhishekkumar54635460@gmail.com>
2022-01-11 01:29:03 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+
+import Cart from "./pages/Cart";
+import Home from "./pages/Home"
+import Login from "./pages/Login";
+import ProductPage from "./pages/ProductPage";
+import Register from "./pages/Register";
+
 const App = () => {
-  return <div>Hello world!</div>;
+  return(
+    <Home />
+  )
 };
 
 export default App;
